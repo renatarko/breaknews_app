5a67ebb022b4aea8e2c915da281ffff4ec05d8b0
Renata Karolina <renatakarolinarko@gmail.com>
2023-03-05 18:18:16 -0400

    created moka datas and folders to components and pages

---
+++ datas.js
@@ -0,0 +1,50 @@
+export default news = [
+  {
+    title: "Noticia 1",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam soluta architecto totam dignissimos? Possimus commodi repudiandae doloremque. Commodi soluta corporis delectus saepe facere nostrum cumque molestias. Mollitia libero illum ipsam!",
+    image:
+      "https://ogimg.infoglobo.com.br/in/13664035-144-725/FT1086A/lorem-ipsum-logo.jpg",
+    likes: 2,
+    comments: 1,
+  },
+  {
+    title: "Noticia 2",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam soluta architecto totam dignissimos? Possimus commodi repudiandae doloremque. Commodi soluta corporis delectus saepe facere nostrum cumque molestias. Mollitia libero illum ipsam!",
+    image:
+      "https://ogimg.infoglobo.com.br/in/13664035-144-725/FT1086A/lorem-ipsum-logo.jpg",
+    likes: 2,
+    comments: 1,
+  },
+  {
+    title: "Noticia 3",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam soluta architecto totam dignissimos? Possimus commodi repudiandae doloremque. Commodi soluta corporis delectus saepe facere nostrum cumque molestias. Mollitia libero illum ipsam!",
+    image:
+      "https://ogimg.infoglobo.com.br/in/13664035-144-725/FT1086A/lorem-ipsum-logo.jpg",
+    likes: 2,
+    comments: 1,
+  },
+  {
+    title: "Noticia 4",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam soluta architecto totam dignissimos? Possimus commodi repudiandae doloremque. Commodi soluta corporis delectus saepe facere nostrum cumque molestias. Mollitia libero illum ipsam!",
+    image:
+      "https://ogimg.infoglobo.com.br/in/13664035-144-725/FT1086A/lorem-ipsum-logo.jpg",
+    likes: 2,
+    comments: 1,
+  },
+  {
+    title: "Noticia 5",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam soluta architecto totam dignissimos? Possimus commodi repudiandae doloremque. Commodi soluta corporis delectus saepe facere nostrum cumque molestias. Mollitia libero illum ipsam!",
+    image:
+      "https://ogimg.infoglobo.com.br/in/13664035-144-725/FT1086A/lorem-ipsum-logo.jpg",
+    likes: 2,
+    comments: 1,
+  },
+  {
+    title: "Noticia 6",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam soluta architecto totam dignissimos? Possimus commodi repudiandae doloremque. Commodi soluta corporis delectus saepe facere nostrum cumque molestias. Mollitia libero illum ipsam!",
+    image:
+      "https://ogimg.infoglobo.com.br/in/13664035-144-725/FT1086A/lorem-ipsum-logo.jpg",
+    likes: 2,
+    comments: 1,
+  },
+];

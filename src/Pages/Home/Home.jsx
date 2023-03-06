2ec981de216376b2e2d93eff290ba0e75f7680cf
Renata Karolina <renatakarolinarko@gmail.com>
2023-03-05 21:47:11 -0400

    updated styles to component in home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,18 @@
+import { Card } from "../../components/Cards/Card";
 import { Navbar } from "../../components/Navbar/Navbar";
+import { news } from "../../../datas";
+import { HomeBody } from "./HomeStyles";
 
 export function Home() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+
+      <HomeBody>
+        {news.map((item, index) => (
+          <Card key={index} news={item} />
+        ))}
+      </HomeBody>
+    </>
+  );
 }
---
+++ src/Pages/Home/HomeStyles.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const HomeBody = styled.section`
+  width: 80%;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 1rem;
+  padding: 1rem 0;
+`;

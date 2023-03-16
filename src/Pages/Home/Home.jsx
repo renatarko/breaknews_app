5e4ae0a2767571415148239149455fe8858acd54
Renata Karolina <renatakarolinarko@gmail.com>
2023-03-15 21:57:45 -0400

    out/cursor: remove the direct call of components

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,17 +5,26 @@ import { HomeBody } from "./HomeStyles";
 import { Footer } from "../../components/Footer/Footer";
 import { SignIn } from "../../components/SignIn/SignIn";
 import { NewAccount } from "../../components/NewAccount/NewAccount";
+import { useEffect, useState } from "react";
 
 export function Home() {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/news")
+      .then((response) => response.json())
+      .then((data) => {
+        setNews(data.results);
+      })
+      .catch((error) => error.message);
+  }, []);
+
   return (
     <>
-      <Navbar />
-      {/* <SignIn /> */}
-      <NewAccount />
       <HomeBody>
-        {news.map((item, index) => (
-          <Card key={index} news={item} />
-        ))}
+        {news.map((item) => {
+          return <Card key={item.id} news={item} />;
+        })}
       </HomeBody>
       <Footer />
     </>

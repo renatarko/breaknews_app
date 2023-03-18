1ed234349c8678f17d8483af5cd7fe94013ac2cb
Renata Karolina <renatakarolinarko@gmail.com>
2023-03-17 21:50:45 -0400

    out/cursor: setting the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import "./App.css";
 import { Home } from "./Pages/Home/Home";
 import { GlobalStyled } from "./GlobalStyled";
+import { Profile } from "./Pages/Profile/Profile";
+import { NewNews } from "./components/NewNews/NewNews";
+import { EditNews } from "./components/EditNews/EditNews";
+
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/Navbar/Navbar";
+import { SignIn } from "./components/SignIn/SignIn";
 
 function App() {
   return (
     <>
-      <GlobalStyled />
-      <Home />
+      <Router>
+        <GlobalStyled />
+
+        {/* <Navbar buttonType="userLogout" /> */}
+
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/profile" Component={Profile} />
+          <Route path="/profile/:id" Component={Profile} />
+          <Route path="/sign" Component={SignIn} />
+          <Route path="*" element={<h1>Pagina não encontrada</h1>} />
+        </Routes>
+      </Router>
     </>
   );
 }

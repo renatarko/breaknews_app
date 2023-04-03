9ea6c75c855be2a7b37e6c583d9a0db8ef0c2295
Renata Karolina <renatakarolinarko@gmail.com>
2023-04-03 09:16:01 -0400

    created context to input search

---
+++ src/context/searchContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from "react";
+
+const SearchContext = createContext();
+
+export const SearchProvider = ({ children }) => {
+  const [inputSearch, setInputSearch] = useState();
+
+  return (
+    <SearchContext.Provider value={{ inputSearch, setInputSearch }}>
+      {children}
+    </SearchContext.Provider>
+  );
+};
+
+export const useSearch = () => {
+  const context = useContext(SearchContext);
+
+  if (!context) {
+    throw new Error("useSearch deve ser usado dentro de um SearchProvider");
+  }
+  return context;
+};

4a7cffe4b5e273567e295ab024f913de2f682470
Renata Karonlina <renatakarolinarko@gmail.com>
2023-05-31 20:05:59 -0400

    show loader for when getting news from api

---
+++ src/Components/Loader/index.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import ContentLoader from "react-content-loader";
+
+const Loader = (props) => (
+  <>
+    <ContentLoader
+      speed={2}
+      width={476}
+      height={224}
+      viewBox="0 0 476 124"
+      backgroundColor="#f3f3f3"
+      foregroundColor="#ecebeb"
+      {...props}
+    >
+      <rect x="14" y="83" rx="3" ry="3" width="88" height="6" />
+      <rect x="13" y="96" rx="3" ry="3" width="52" height="6" />
+      <rect x="7" y="19" rx="3" ry="3" width="320" height="5" />
+      <rect x="7" y="34" rx="3" ry="3" width="304" height="5" />
+      <rect x="7" y="49" rx="3" ry="3" width="178" height="6" />
+      <rect x="344" y="10" rx="0" ry="0" width="89" height="80" />
+    </ContentLoader>
+
+    <ContentLoader
+      speed={2}
+      width={476}
+      height={224}
+      viewBox="0 0 476 124"
+      backgroundColor="#f3f3f3"
+      foregroundColor="#ecebeb"
+      {...props}
+    >
+      <rect x="14" y="83" rx="3" ry="3" width="88" height="6" />
+      <rect x="13" y="96" rx="3" ry="3" width="52" height="6" />
+      <rect x="7" y="19" rx="3" ry="3" width="320" height="5" />
+      <rect x="7" y="34" rx="3" ry="3" width="304" height="5" />
+      <rect x="7" y="49" rx="3" ry="3" width="178" height="6" />
+      <rect x="344" y="10" rx="0" ry="0" width="89" height="80" />
+    </ContentLoader>
+
+    <ContentLoader
+      speed={2}
+      width={476}
+      height={224}
+      viewBox="0 0 476 124"
+      backgroundColor="#f3f3f3"
+      foregroundColor="#ecebeb"
+      {...props}
+    >
+      <rect x="14" y="83" rx="3" ry="3" width="88" height="6" />
+      <rect x="13" y="96" rx="3" ry="3" width="52" height="6" />
+      <rect x="7" y="19" rx="3" ry="3" width="320" height="5" />
+      <rect x="7" y="34" rx="3" ry="3" width="304" height="5" />
+      <rect x="7" y="49" rx="3" ry="3" width="178" height="6" />
+      <rect x="344" y="10" rx="0" ry="0" width="89" height="80" />
+    </ContentLoader>
+
+    <ContentLoader
+      speed={2}
+      width={476}
+      height={224}
+      viewBox="0 0 476 124"
+      backgroundColor="#f3f3f3"
+      foregroundColor="#ecebeb"
+      {...props}
+    >
+      <rect x="14" y="83" rx="3" ry="3" width="88" height="6" />
+      <rect x="13" y="96" rx="3" ry="3" width="52" height="6" />
+      <rect x="7" y="19" rx="3" ry="3" width="320" height="5" />
+      <rect x="7" y="34" rx="3" ry="3" width="304" height="5" />
+      <rect x="7" y="49" rx="3" ry="3" width="178" height="6" />
+      <rect x="344" y="10" rx="0" ry="0" width="89" height="80" />
+    </ContentLoader>
+  </>
+);
+
+export default Loader;

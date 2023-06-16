028a11a62fc59ccccd932cbcbd0a06cd1bb40b7c
Renata Karonlina <renatakarolinarko@gmail.com>
2023-06-15 21:41:12 -0400

    created route to sign in and chandeg id to username

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { Navbar } from "./Components/Navbar/index";
 import { GlobalStyled } from "./GlobalStyled";
 import { Home } from "./Pages/Home/index";
 import { Profile } from "./Pages/Profile/index";
 
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Navbar } from "./Components/Navbar";
+import { SignIn } from "./Components/SignIn";
 import { AuthProvider } from "./Context/authContext";
 import { SearchProvider } from "./Context/searchContext";
 
@@ -12,14 +13,14 @@ function App() {
     <>
       <Router>
         <GlobalStyled />
-
         <AuthProvider>
           <SearchProvider>
-            <Navbar buttonType="userLogout" />
+            <Navbar />
             <Routes>
               <Route path="breaknews_app" Component={Home} />
               {/* <Route path="breaknews_app/profile" Component={Profile} /> */}
-              <Route path="breaknews_app/profile/:id" Component={Profile} />
+              <Route path="breaknews_app/profile/:name" Component={Profile} />
+              <Route path="breaknews_app/login" Component={SignIn} />
               {/* <Route path="breaknews_app/search/:search" Component={Search} /> */}
               <Route path="*" element={<h1>Pagina não encontrada</h1>} />
             </Routes>

8d26392b0ea46efeef1214be657508b9c2c918a5
Renata Karolina <renatakarolinarko@gmail.com>
2023-06-22 14:06:54 -0400

    updated on error messages

--- src/Components/SignIn/index.jsx
+++ src/Components/SignIn/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { toast } from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../Context/authContext";
 import { NewAccount } from "../NewAccount/index";
 
@@ -8,20 +8,23 @@ import { Lock, Mail } from "lucide-react";
 import { Button } from "../Button";
 import { Form } from "../Form";
 import { Input } from "../Input";
+import ClipLoader from "react-spinners/ClipLoader";
 import { Modal } from "../Modal";
+
+import * as S from "./styles";
 
 export function SignIn() {
   const [open, setOpen] = useState(true); //abrir modal de login
   const [openNew, setOpenNew] = useState(false); // abrir modal para cadastrar usuário
-
-  const { signIn, messageError } = useAuth();
-
-  const navigative = useNavigate();
-
   const [userLogin, setUserLogin] = useState({
     email: "",
     password: "",
   });
+  const [loading, setLoading] = useState(false);
+
+  const { signIn, messageError, setMessageError } = useAuth();
+
+  const navigative = useNavigate();
 
   function handleChangeInput(e) {
     const { name, value } = e.target;
@@ -33,15 +36,20 @@ export function SignIn() {
     e.preventDefault();
 
     if (!userLogin.email || !userLogin.password) {
-      return toast.error("Preencha os campos!");
+      return setMessageError("Preencha os campos para fazer Login.");
     }
 
     try {
+      setLoading(true);
       const user = await signIn(userLogin);
-      // setOpen(false);
-      navigative(`/breaknews_app/profile/${user.username}`);
+
+      if (user) {
+        return navigative(`/breaknews_app/profile`);
+      }
     } catch (error) {
-      console.log("error", error);
+      console.log("error", error.message);
+    } finally {
+      setLoading(false);
     }
   }
 
@@ -58,31 +66,44 @@ export function SignIn() {
     <>
       {/* <Toaster /> */}
       {open ? (
-        <Modal>
-          <Form
-            title="Entrar"
-            handleClick={() => setOpen(false)}
-            isNoAccount
-            handleOpen={handleOpenNewAccount}
-          >
-            <Input
-              icon={<Mail />}
-              type="email"
-              placeholder="E-mail"
-              name="email"
-              handleChange={handleChangeInput}
-            />
+        // <Modal>
+        <Form
+          title="Entrar"
+          handleClick={() => setOpen(false)}
+          isNoAccount
+          handleOpen={handleOpenNewAccount}
+        >
+          <Input
+            icon={<Mail />}
+            type="email"
+            placeholder="E-mail"
+            name="email"
+            handleChange={handleChangeInput}
+            onFocus={() => setMessageError("")}
+          />
 
-            <Input
-              icon={<Lock />}
-              type="password"
-              placeholder="Senha"
-              name="password"
-              handleChange={handleChangeInput}
-            />
-            <Button handleClick={fazerLogin}>enviar</Button>
-          </Form>
-        </Modal>
+          <Input
+            icon={<Lock />}
+            type="password"
+            placeholder="Senha"
+            name="password"
+            handleChange={handleChangeInput}
+          />
+          <S.Error>{messageError}</S.Error>
+          <Button handleClick={fazerLogin}>
+            {loading ? <ClipLoader color="#fff" size={16} /> : "enviar"}
+          </Button>
+
+          <S.ContainerNewAccount>
+            <p>Não tem uma conta?</p>
+            <Link
+              to="/breaknews_app/sign-up"
+              style={{ textDecoration: "none" }}
+            >
+              <S.ButtonCreatedUser>Cadastre-se</S.ButtonCreatedUser>
+            </Link>
+          </S.ContainerNewAccount>
+        </Form>
       ) : null}
     </>
   );

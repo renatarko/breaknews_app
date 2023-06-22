de2e6e083fe17605967a5122c5717d4d6a720229
Renata Karolina <renatakarolinarko@gmail.com>
2023-06-22 14:06:12 -0400

    removed the call to create new account

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,25 +1,12 @@
 import { X } from "lucide-react";
 import * as S from "./styles";
 
-export const Form = ({
-  title,
-  children,
-  handleClick,
-  isNoAccount,
-  handleOpen,
-}) => {
+export const Form = ({ title, children, handleClick }) => {
   return (
     <S.Wrapper>
       <X className="btn-close" onClick={handleClick} />
       <S.Title>{title}</S.Title>
       {children}
-
-      {isNoAccount && (
-        <S.ContainerNewAccount>
-          <p>Não tem uma conta?</p>
-          <button onClick={handleOpen}>Cadastre-se</button>
-        </S.ContainerNewAccount>
-      )}
     </S.Wrapper>
   );
 };
